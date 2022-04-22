951a99ed7db81ea49c9498b50fa9b1bdbfae011f
Farruh Olimov <farruhbekolimov3904@gmail.com>
2022-04-22 15:31:15 +0500

    update

--- src/modules/products/products.service.ts
+++ src/modules/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CategoriesService } from 'src/modules/categories/categories.service';
 import { Categories } from 'src/modules/categories/entities/category.entity';
@@ -15,30 +15,44 @@ export class ProductsService {
 
   async create(product: CreateProductInput) {
     let p = this.productRepository.create(product);
+
+    if(!p) throw new NotFoundException("Something went wrong")
+
     return this.productRepository.save(p)
   }
 
-  async findAll() {
+  async findAll(): Promise<Products[]> {
     return this.productRepository.find({
       relations: ["images"]
     });
   }
 
-  async findOne(id: string) {
-    return this.productRepository.findOne(id, {
+  async findOne(id: string): Promise<Products> {
+    const p = await this.productRepository.findOne(id, {
       relations: ["images"]
     });
+
+    if(!p) throw new NotFoundException("Product not found")
+
+    return p;
   }
 
-  async update(id: string, product: UpdateProductInput) {
-    return this.productRepository.update(id, product);
+  async update(id: string, product: UpdateProductInput): Promise<Products> {
+    const p = await this.productRepository.preload({
+      id: id,
+      ...product
+    });
+
+    if(!p) throw new NotFoundException("Product not found")
+
+    return p;
   }
 
   async remove(id: string) {
-    return this.productRepository.delete(id);
+    return await this.productRepository.delete(id);
   }
 
   async getCategory(id: string): Promise<Categories>{
-    return this.categoryService.findOne(id)
+    return await this.categoryService.findOne(id)
   }
 }
